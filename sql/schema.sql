f71a5cec03bc995ba6354bb9f7e6fdd2457ad6f0
gaius1304 <gaius1304@protonmail.com>
2019-09-14 19:00:51 -0700

    Add alliance_messages table to schema
    This should be added when improved alliance messages were added, but I
    forgot about that.

--- sql/schema.sql
+++ sql/schema.sql
@@ -522,6 +522,19 @@ create index private_messages_sender_id_deleted_by_sender_at_idx
 create index private_messages_recipient_id_deleted_by_recipient_at_idx
           on private_messages (recipient_id, deleted_by_recipient, at desc);
 
+-- Alliance messages
+
+create table alliance_messages (
+  id bigserial primary key,
+  alliance_id bigint references alliances on delete cascade not null,
+  sender_id bigint references users on delete set null,
+  at timestamptz not null,
+  message text not null
+);
+
+create index alliance_messages_alliance_id_at_idx
+          on alliance_messages (alliance_id, at desc);
+
 -- Broadcast messages
 
 create table broadcast_messages (
