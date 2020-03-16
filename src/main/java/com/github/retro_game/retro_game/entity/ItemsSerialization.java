e316d868997323cc1e78543c792fcadf9703cc8f
Patryk Stefanski <patryk.stefanski@protonmail.com>
2020-03-16 16:07:59 +0100

    Rename enumValues to enumConstants

--- src/main/java/com/github/retro_game/retro_game/entity/ItemsSerialization.java
+++ src/main/java/com/github/retro_game/retro_game/entity/ItemsSerialization.java
@@ -5,11 +5,11 @@ import java.util.Map;
 
 class ItemsSerialization {
   static <K extends Enum<K>> EnumMap<K, Integer> deserializeItems(Class<K> clazz, int[] array) {
-    var enumValues = clazz.getEnumConstants();
-    assert enumValues.length == array.length;
+    var enumConstants = clazz.getEnumConstants();
+    assert enumConstants.length == array.length;
     var items = new EnumMap<K, Integer>(clazz);
     for (var i = 0; i < array.length; i++) {
-      var kind = enumValues[i];
+      var kind = enumConstants[i];
       var value = array[i];
       assert value >= 0;
       items.put(kind, value);
@@ -18,8 +18,8 @@ class ItemsSerialization {
   }
 
   static <K extends Enum<K>> int[] serializeItems(Class<K> clazz, Map<K, Integer> items) {
-    var enumValues = clazz.getEnumConstants();
-    var array = new int[enumValues.length];
+    var enumConstants = clazz.getEnumConstants();
+    var array = new int[enumConstants.length];
     for (var entry : items.entrySet()) {
       var index = entry.getKey().ordinal();
       var value = entry.getValue();
