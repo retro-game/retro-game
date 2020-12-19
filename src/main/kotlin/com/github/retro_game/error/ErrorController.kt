f91b7a758a325fe0162f725f099a784d5c19fc48
lsrom <lukas.srom@gmail.com>
2020-12-19 20:52:03 +0100

    Add page on error with links to Github and Discord so users can report bugs or get support

---
+++ src/main/kotlin/com/github/retro_game/error/ErrorController.kt
@@ -0,0 +1,11 @@
+package com.github.retro_game.error
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class ErrorController {
+
+    @GetMapping("/error")
+    fun error() = "error"
+}
