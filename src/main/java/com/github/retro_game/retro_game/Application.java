7f0663f6ac8a137dab9fe0e1ad418d6db6b19640
lsrom <lukas.srom@gmail.com>
2020-12-20 17:17:03 +0100

    Add com.github.retro_game as base package for injection

--- src/main/java/com/github/retro_game/retro_game/Application.java
+++ src/main/java/com/github/retro_game/retro_game/Application.java
@@ -3,7 +3,7 @@ package com.github.retro_game.retro_game;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.github.retro_game.*"})
 public class Application {
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
