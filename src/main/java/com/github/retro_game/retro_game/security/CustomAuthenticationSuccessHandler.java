bc3f0ce6c812b8d666a1c0eab8a31e7f22284421
gaius1304 <gaius1304@protonmail.com>
2021-12-13 21:20:00 +0100

    Evict cache during login

--- src/main/java/com/github/retro_game/retro_game/security/CustomAuthenticationSuccessHandler.java
+++ src/main/java/com/github/retro_game/retro_game/security/CustomAuthenticationSuccessHandler.java
@@ -1,7 +1,7 @@
 package com.github.retro_game.retro_game.security;
 
-import com.github.retro_game.retro_game.entity.FreeSystem;
-import com.github.retro_game.retro_game.entity.User;
+import com.github.retro_game.retro_game.cache.BodyInfoCache;
+import com.github.retro_game.retro_game.cache.UserInfoCache;
 import com.github.retro_game.retro_game.repository.FreeSystemRepository;
 import com.github.retro_game.retro_game.repository.UserRepository;
 import com.github.retro_game.retro_game.service.exception.NoMoreFreeSystemsException;
@@ -15,15 +15,19 @@ import org.springframework.transaction.annotation.Transactional;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Optional;
 
 @Component
 public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
   private static final Logger logger = LoggerFactory.getLogger(CustomAuthenticationSuccessHandler.class);
+  private final BodyInfoCache bodyInfoCache;
+  private final UserInfoCache userInfoCache;
   private final FreeSystemRepository freeSystemRepository;
   private final UserRepository userRepository;
 
-  public CustomAuthenticationSuccessHandler(FreeSystemRepository freeSystemRepository, UserRepository userRepository) {
+  public CustomAuthenticationSuccessHandler(BodyInfoCache bodyInfoCache, UserInfoCache userInfoCache,
+                                            FreeSystemRepository freeSystemRepository, UserRepository userRepository) {
+    this.bodyInfoCache = bodyInfoCache;
+    this.userInfoCache = userInfoCache;
     this.freeSystemRepository = freeSystemRepository;
     this.userRepository = userRepository;
   }
@@ -32,15 +36,23 @@ public class CustomAuthenticationSuccessHandler implements AuthenticationSuccess
   @Transactional(readOnly = true)
   public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
                                       Authentication authentication) throws IOException {
-    CustomUser customUser = (CustomUser) authentication.getPrincipal();
+    var customUser = (CustomUser) authentication.getPrincipal();
     logger.info("User auth: userId={} ip={}", customUser.getUserId(), request.getRemoteAddr());
-    User user = userRepository.getOne(customUser.getUserId());
-    Optional<Long> homeworldId = user.getBodies().keySet().stream().min(Long::compareTo);
+
+    var user = userRepository.getById(customUser.getUserId());
+    var bodies = user.getBodies();
+
+    // Evict cache.
+    userInfoCache.evict(customUser.getUserId());
+    for (var bodyId : bodies.keySet())
+      bodyInfoCache.evict(bodyId);
+
+    var homeworldId = bodies.keySet().stream().min(Long::compareTo);
     if (homeworldId.isPresent()) {
       response.sendRedirect(String.format("/overview?body=%d", homeworldId.get()));
       return;
     }
-    FreeSystem freeSystem = freeSystemRepository.findFirstBy();
+    var freeSystem = freeSystemRepository.findFirstBy();
     if (freeSystem == null) {
       throw new NoMoreFreeSystemsException();
     }
